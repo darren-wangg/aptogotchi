584b59dad8abcfaafa4b37a254505e8e7f475913
darren wang <darren@aptoslabs.com>
2023-09-05 15:53:27 -0700

    package errors

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -19,20 +19,6 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
     <html lang="en">
-      <Head>
-        <link
-          href="https://unpkg.com/nes.css@2.3.0/css/nes.min.css"
-          rel="stylesheet"
-        />
-        <link
-          href="https://unpkg.com/nes.css@latest/css/nes.min.css"
-          rel="stylesheet"
-        />
-        <link
-          href="https://unpkg.com/nes.css/css/nes-core.min.css"
-          rel="stylesheet"
-        />
-      </Head>
       <body className={kongtext.className}>
         <WalletProvider>{children}</WalletProvider>
       </body>
